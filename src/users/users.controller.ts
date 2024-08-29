e8c40a059375949415537fd899e66756c49e4bc2
akdoganalibugra1 <akdoganalibugra@gmail.com>
2024-08-29 18:34:12 +0300

    Refactor UsersController to use UsersService for CRUD operations

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,38 +1,56 @@
 import {
-  Body,
-  Controller,
-  Delete,
-  Get,
-  Param,
-  Patch,
-  Post,
-  Query,
+    Body,
+    Controller,
+    Delete,
+    Get,
+    Param,
+    Patch,
+    Post,
+    Query,
 } from '@nestjs/common';
+import { UsersService } from './users.service';
 
 @Controller('users')
 export class UsersController {
-  @Get()
-  findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
-    return [];
-  }
+    constructor(private readonly usersService: UsersService) {}
 
-  @Get(':id')
-  findOne(@Param('id') id: string) {
-    return { id };
-  }
+    @Get()
+    findAll(@Query('role') role?: 'INTERN' | 'ENGINEER' | 'ADMIN') {
+        return this.usersService.findAll(role);
+    }
 
-  @Post()
-  create(@Body() user: {}) {
-    return user;
-  }
+    @Get(':id')
+    findOne(@Param('id') id: string) {
+        return this.usersService.findOne(+id);
+    }
 
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() userUpdate: {}) {
-    return { id, ...userUpdate };
-  }
+    @Post()
+    create(
+        @Body()
+        user: {
+            name: string;
+            email: string;
+            role: 'INTERN' | 'ENGINEER' | 'ADMIN';
+        },
+    ) {
+        return this.usersService.create(user);
+    }
 
-  @Delete(':id')
-  delete(@Param('id') id: string) {
-    return { id };
-  }
+    @Patch(':id')
+    update(
+        @Param('id') id: string,
+        @Body()
+        userUpdate: {
+            name?: string;
+            email?: string;
+            role?: 'INTERN' | 'ENGINEER' | 'ADMIN';
+        },
+    ) {
+        return this.usersService.update(+id, userUpdate);
+    }
+
+    @Delete(':id')
+    delete(@Param('id') id: string) {
+        return this.usersService.delete(+id);
+    }
 }
